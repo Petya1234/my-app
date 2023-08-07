6cb0302d1fc5b57635acf58dcc76cb015387e6dc
Petya1234 <mr.alekseenko2018@bk.ru>
2023-08-07 11:03:55 +0500

    all ok commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
-import Image from 'next/image';
-import imgSrc from '../public/photos/1photo.jpg';
-import '@/app/main-content-styles.css'
+import Image from "next/image";
+import imgSrc from "../public/photos/1photo.jpg";
+import "@/app/main-content-styles.css";
+import { Component } from "react";
+
+
 
 export default function Home() {
   return (
     <div>
-      <Image className='main-img' src={imgSrc} alt="" role="presentation" />
+      <Image className="main-img" src={imgSrc} alt="" />
+      <div className="main-text-content">
+        <h1>
+          Добро пожаловать на сайт о Пете Алексеенко <br />
+          Здесь вы сможете узнать немного его биографии
+        </h1>
+      </div>
     </div>
-  )
+  );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -4,9 +4,9 @@ import './styles.css'
 const Header = () => {
     return (
         <header className="header">
-           <Link href= "/">Home</Link>
-           <Link href= "/blog">Blog</Link>
-           <Link href= "/contacts">Contacts</Link>
+           <Link href= "/">Главная</Link>
+           <Link href= "/blog">Факты</Link>
+           <Link href= "/contacts">Написать мне</Link>
         </header>
     )
 }
