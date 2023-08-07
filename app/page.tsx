0cade6a9cbcd9650aa45d2526fbe8e474058147a
Petya1234 <mr.alekseenko2018@bk.ru>
2023-08-07 15:35:13 +0500

    all works

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+'use client'
 import Image from "next/image";
 import imgSrc from "../public/photos/1photo.jpg";
 import "@/app/main-content-styles.css";
-import { Component } from "react";
+import { EmblaCarousel } from "@/components/EmblaCarousel";
 
 
+function handleClick() {
+  console.log('button clicked')
+}
 
 export default function Home() {
   return (
@@ -14,6 +18,8 @@ export default function Home() {
           Добро пожаловать на сайт о Пете Алексеенко <br />
           Здесь вы сможете узнать немного его биографии
         </h1>
+        <button className="button" onClick={handleClick}>Подробности</button>
+        <EmblaCarousel/>
       </div>
     </div>
   );
---
+++ components/EmblaCarousel.tsx
@@ -0,0 +1,27 @@
+import './styles.css'
+import Image from 'next/image'
+import React from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+import imgCarousel1 from '@/public/photos/boots-photo.jpg'
+import imgCarousel2 from '@/public/photos/boys-photo.jpg'
+import imgCarousel3 from '@/public/photos/gym-photo.jpg'
+import imgCarousel4 from '@/public/photos/cat-photo.jpg'
+
+export const EmblaCarousel = () => {
+  const [emblaRef] = useEmblaCarousel()
+
+  return (
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container">
+        <div className="embla__slide"><Image height={1000} width={1500} src={imgCarousel1} alt=''></Image>
+        <p>Мои новые кроссики</p></div>
+        <div className="embla__slide"><Image height={1000} width={1500} src={imgCarousel2} alt=''></Image>
+        <p>Лагерь</p></div>
+        <div className="embla__slide"><Image height={1000} width={1500} src={imgCarousel3} alt=''></Image>
+        <p>Любимый зал</p></div>
+        <div className="embla__slide"><Image height={1000} width={1500} src={imgCarousel4} alt=''></Image>
+        <p>Кошка???</p></div>
+      </div>
+    </div>
+  )
+}
