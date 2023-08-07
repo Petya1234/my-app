8db2a816a77a69f592ec61e94ea1b17dc9c66b9b
Petya1234 <mr.alekseenko2018@bk.ru>
2023-08-07 09:54:25 +0500

    add gradient

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Image from 'next/image';
+import imgSrc from '../public/photos/1photo.jpg';
+import '@/app/main-content-styles.css'
 
 export default function Home() {
   return (
     <div>
-      
-      <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, exercitationem facilis? Exercitationem eligendi quam laborum dolore, placeat voluptates ipsum facilis temporibus impedit maxime, atque ut repudiandae architecto reprehenderit minima suscipit!</p>
+      <Image className='main-img' src={imgSrc} alt="" role="presentation" />
     </div>
   )
 }
