38b5141ba3fcd333b2480cc4e8efae02dc105b09
Petya1234 <mr.alekseenko2018@bk.ru>
2023-08-08 11:14:54 +0500

    css improve

--- components/EmblaCarousel.tsx
+++ components/EmblaCarousel.tsx
@@ -21,7 +21,7 @@ export const EmblaCarousel = () => {
         <div className="embla__slide"><Image height={1000} width={1000} src={imgCarousel3} alt=''></Image>
         <p>Любимый зал</p></div>
         <div className="embla__slide"><Image height={1000} width={1000} src={imgCarousel4} alt=''></Image>
-        <p className='cat-paragraph'>Кошка???</p><p><Link className='facts-link' href='/facts'> Перейти к фактам </Link></p></div>
+        <p className='cat-paragraph'>Кошка???</p><p><Link className='facts-link' href='/facts'> Перейти к фактам</Link></p></div>
       </div>
     </div>
   )
