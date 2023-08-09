f56b27136cef6071eff7a7d6257cf2ad6051e5f4
Petya1234 <mr.alekseenko2018@bk.ru>
2023-08-09 11:07:46 +0500

    css

--- app/facts/page.tsx
+++ app/facts/page.tsx
@@ -2,13 +2,24 @@ import './styles.css'
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: 'Facts'
-  };
+  title: 'Facts'
+};
 
 
 
 export default function Facts() {
   return (
-    <div className="main-facts-content"></div>
+    <div className="main-facts-content">
+      <h1 className='welcome-content'>Добро пожаловать на страницу фактов обо мне</h1>
+      <div className='facts'>
+        <ul className="border">
+          <li>Я ценю в людях честность превыше всего, но я не против того, чтобы люди врали там, где не вредит человеку морально или же материально.</li>
+          <li></li>
+          <li>Элемент списка</li>
+          <li>Элемент списка</li>
+          <li>Элемент списка</li>
+        </ul>
+      </div>
+    </div>
   )
 }
