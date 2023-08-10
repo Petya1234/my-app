a9d03b26450746b07213145126add7ba0dc6214a
Petya1234 <mr.alekseenko2018@bk.ru>
2023-08-10 11:34:06 +0500

    css improvments

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,10 +1,10 @@
 import { Metadata } from "next";
 import Image from "next/image";
-import './styles.css'
-import imgTelegram from '@/public/photos/telegram-photo.jpg'
-import imgTelegramQr from '@/public/photos/telegram-qr-photo.png'
-import ImgVk from '@/public/photos/vk-photo.jpg'
-import ImgVkQr from '@/public/photos/vk-qr-photo.jpg'
+import "./styles.css";
+import imgTelegram from "@/public/photos/telegram-photo.jpg";
+import imgTelegramQr from "@/public/photos/telegram-qr-photo.png";
+import ImgVk from "@/public/photos/vk-photo.jpg";
+import ImgVkQr from "@/public/photos/vk-qr-photo.jpg";
 
 export const metadata: Metadata = {
   title: "Contacts",
@@ -14,15 +14,24 @@ export default function Contacts() {
   return (
     <div className="main-div">
       <div className="box">
-        <div className="img-box-tg"><Image width={150} height={150} src={imgTelegram} alt=''></Image></div>
-        <Image className="telegram-qr" src = {imgTelegramQr} width={300} height={300} alt=''></Image>
-        <p className="link-inscr-tg">@QWRTTQR</p>
+        <ul>
+          <div className="img-box-tg">
+            <Image width={150} height={150} src={imgTelegram} alt=""></Image>
+          </div>
+          <div className="telegram-qr">
+            <Image src={imgTelegramQr} width={300} height={300} alt=""></Image>
+          </div>
+          <p className="link-inscr-tg">@QWRTTQR</p>
+        </ul>
       </div>
       <div className="box">
-      <div className="img-box-vk"><Image width={150} height={150} src={ImgVk} alt=''></Image></div>
-      <Image className="vk-qr" src = {ImgVkQr} width={300} height={300} alt=''></Image>
-      <p className="link-inscr-vk">VK.COM/QWRTTQ</p>
-  
+        <ul>
+          <div className="img-box-vk">
+            <Image width={150} height={150} src={ImgVk} alt=""></Image>
+          </div>
+          <div className="vk-qr"><Image  src={ImgVkQr} width={300} height={300} alt=""></Image> </div>
+          <p className="link-inscr-vk">VK.COM/QWRTTQ</p>
+        </ul>
       </div>
       <div className="box">
         <p>
