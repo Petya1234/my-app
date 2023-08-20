2e382ec3a706167ea3896276f740296587d686e0
Petya1234 <mr.alekseenko2018@bk.ru>
2023-08-20 11:30:17 +0500

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,6 @@ export default function RootLayout({
   return (
     
     <html lang="en">
-      <Head>
-          <link rel="shortcut icon" href='@/public/static/favicon.ico'/>
-      </Head>
       <body>
         <Providers>
         <main className='container' >{children}</main>
